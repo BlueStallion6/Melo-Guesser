4ea94b9ef6722095cd2c9daad64e778e3479fd30
Denis <denissas2014@gmail.com>
2025-02-09 00:26:41 +0200

    commit #1.1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ("hielo")
+try:
+    import sys
+    import json
+    import os
+    import random
+    from keywords import *
+    import colored
+    import spotipy
+
+except ImportError:
+    print("ImportError >> Please run 'pip install -r requirements.txt' in this project's directory.")
+    exit()
+
+#######################################################################################################################
+
+print_success("hello <3")
