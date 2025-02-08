60bf39fa405857e04b0465225ae24078bf932407
Denis <denissas2014@gmail.com>
2025-02-09 01:22:08 +0200

    commit #1.3

--- main.py
+++ main.py
@@ -20,6 +20,40 @@ except ImportError:
 load_dotenv()
 GENIUS_ACCESS_TOKEN = os.getenv('GENIUS_ACCESS_TOKEN')
 if not GENIUS_ACCESS_TOKEN:
-    raise ValueError("Genius API token not found. Please check your .env file.")
+    raise ValueError("Genius API token not found.")
+
+The_Weeknd = "The Weeknd"
+Billie_Eilish = "Billie Eilish"
+Lana_Del_Rey = "Lana Del Rey"
+Tame_Impala = "Tame Impala"
+Olivia_Rodrigo = "Olivia Rodrigo"
 
 #######################################################################################################################
+
+genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN, timeout = 12)
+
+def get_lyrics(title, artist):
+    song = genius.search_song(title, artist)
+    if song:
+        return song.lyrics
+    else:
+        return None
+
+lyrics = get_lyrics("CHIHIRO", Billie_Eilish)
+if lyrics:
+    print(lyrics)
+else:
+    print("Lyrics not found.")
+
+
+
+
+
+
+
+
+
+
+
+
+
